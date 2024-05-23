af6fec97d8f259002e8f9eb5c6fe7601dff0a0f7
Rahul Raj Anand <rahulrajanand7@gmail.com>
2024-05-23 16:41:28 +0530

    Update Footer.jsx

--- ContraHub/src/components/FeedPosts/PostFooter.jsx
+++ ContraHub/src/components/FeedPosts/PostFooter.jsx
@@ -1,8 +1,35 @@
+import { Box, Flex } from "@chakra-ui/react"
+import { useState } from "react"
+import { NotificationsLogo, UnlikeLogo } from "../../assets/constants.jsx";
 
 const PostFooter = () => {
-  return (
-    <div>PostFooter</div>
-  )
+  const [liked, setLiked] = useState(false);
+  const [likes, setLikes] = useState(1000);
+
+  const handleLike = () => {
+    if(liked) {
+      setLiked(false);
+      setLikes(likes - 1);
+    } else {
+      setLiked(true);
+      setLikes(likes + 1);
+    }
+  }
+
+
+  return <>
+
+    <Flex alignItems={"center"} gap={4} w={"full"} pt={0} mb={2} mt={"auto"}>
+      <Box onClick={handleLike}
+      cursor={"pointer"}
+      fontSize={17}
+      >
+        {!liked ? (<NotificationsLogo/>) : (<UnlikeLogo/>)}
+      </Box>
+    </Flex>
+  </>
+    
+  
 }
 
 export default PostFooter
