21ff13fa0c5710aa796890db2a5f2555b9919316
Rahul Raj Anand <rahulrajanand7@gmail.com>
2024-05-24 11:17:40 +0530

    Suggested User done

---
+++ ContraHub/src/components/SuggestedUsers/SuggestedUser.jsx
@@ -0,0 +1,39 @@
+import { Avatar, Box, Button, Flex, VStack } from "@chakra-ui/react"
+import { useState } from "react"
+
+
+const SuggestedUser = ({followers, name, avatar}) => {
+    const [isFollwed, setIsFollwed] = useState(false)
+
+  return (
+    <>
+    <Flex justifyContent={"space-between"} alignItems={"center"} w={"full"}>
+        <Flex alignItems={"center"} gap={2}>
+            <Avatar src={avatar} name={name} size={"md"}/>
+            <VStack spacing={2}>
+                <Box fontSize={12} fontWeight={"bold"}>
+                    {name}
+                </Box>
+                <Box fontSize={11} color={"gray.500"}>
+                    {followers} followers
+                </Box>
+            </VStack>
+        </Flex>
+        <Button
+            fontSize={13}
+            bg={"transparent"}
+            p={0}
+            h={"max-content"}
+            fontWeight={"medium"}
+            color={"blue.500"}
+            _hover={{color:"white"}}
+            onClick={() => setIsFollwed(!isFollwed)}
+        >
+            {isFollwed ? "Unfollow" : "Follow"}
+        </Button>
+    </Flex>
+    </>
+  )
+}
+
+export default SuggestedUser
--- ContraHub/src/pages/HomePage/HomePage.jsx
+++ ContraHub/src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,6 @@
 import { Box, Container, Flex } from '@chakra-ui/react'
 import FeedPosts from '../../components/FeedPosts/FeedPosts'
+import SuggestedUsers from '../../components/SuggestedUsers/SuggestedUsers'
 
 
 const HomePage = () => {
@@ -11,9 +12,8 @@ const HomePage = () => {
         </Box>
         <Box flex={3} mr={20}
           display={{base:"none",lg:"block"}}
-          maxW={"300px"}         
-        >
-          Suggested User
+          maxW={"300px"}         >
+          <SuggestedUsers/>
         </Box>
       </Flex>
     </Container>
