43c4ac60de7e7fb2f36fc84b82d7a3e2144c376d
Rahul Raj Anand <rahulrajanand7@gmail.com>
2024-05-26 12:46:12 +0530

    intial commit

--- ContraHub/src/components/AuthForm/Signup.jsx
+++ ContraHub/src/components/AuthForm/Signup.jsx
@@ -1,5 +1,5 @@
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
-import { Button, Input, InputGroup, InputRightElement} from "@chakra-ui/react"
+import { Alert, AlertIcon,Button, Input, InputGroup, InputRightElement} from "@chakra-ui/react"
 import { useState } from "react"
 import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
 
@@ -38,12 +38,12 @@ const Signup = () => {
         placeholder="Full Name"
         fontSize={14}
         type="text"
-        value={inputs.fullName}
         size={"sm"}
+        value={inputs.fullName}
         onChange={(e) => setInputs({...inputs,fullName:e.target.value})}
     />
     <InputGroup>
-    <Input 
+      <Input 
         placeholder="Password"
         fontSize={14}
         type={showPassword ? "text" : "password"}
@@ -58,7 +58,16 @@ const Signup = () => {
     </InputRightElement>
   </InputGroup>
 
-  <Button w={"full"} colorScheme="blue" size={"sm"} fontSize={14} onClick={() => signup(inputs)}>
+  {error && (
+    <Alert status="error" fontSize={13} p={2} borderRadius={4}>
+        <AlertIcon fontSize={12}/>
+        {error.message}
+    </Alert>
+  )}
+
+  <Button w={"full"} colorScheme="blue" size={"sm"} fontSize={14} 
+    isLoading={loading}
+  onClick={() => signup(inputs)}>
         Sign Up
     </Button>
     
--- ContraHub/src/hooks/useSignUpWithEmailAndPassword.js
+++ ContraHub/src/hooks/useSignUpWithEmailAndPassword.js
@@ -1,5 +1,6 @@
 import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
-import { auth } from '../firebase/firebase';
+import { auth, firestore } from '../firebase/firebase';
+import { doc, setDoc } from 'firebase/firestore';
 
 const useSignUpWithEmailAndPassword = () => {
     const [
@@ -15,20 +16,32 @@ const useSignUpWithEmailAndPassword = () => {
             return
         }
         try {
-          const  newUser = await createUserWithEmailAndPassword(inputs.email,inputs.password)
+          const  newUser = await createUserWithEmailAndPassword(inputs.email,inputs.password);
           if(!newUser && error) {
-            console.log(error)
-            return
+            console.log(error);
+            return;
         }
           if(newUser){
-
+            const userDoc = {
+              uid: newUser.user.id,
+              email:inputs.email,
+              username:inputs.username,
+              fullName:inputs.fullName,
+              bio:"",
+              profilePicURL:"",
+              followers:[],
+              following:[],
+              posts:[],
+              createdAt:Date.now(),
+            }
+            await setDoc(doc(firestore, "users", newUser.user.uid), userDoc);
+            localStorage.setItem("user-info",JSON.stringify(userDoc));
           }
         } catch (error) {
             console.log(error);
         }
-      }
-
+      } 
       return {loading, error , signup}
 }
 
-export default useSignUpWithEmailAndPassword
+export default useSignUpWithEmailAndPassword;
