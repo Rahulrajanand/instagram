27a12a7c8a4c06b392653c3df9ddcff03ca90fe6
Rahul Raj Anand <rahulrajanand7@gmail.com>
2024-05-27 10:59:43 +0530

    initial commit

--- ContraHub/src/hooks/useLogin.js
+++ ContraHub/src/hooks/useLogin.js
@@ -7,10 +7,10 @@ import useAuthStore from "../store/authStore";
 const useLogin = () => {
     const showToast = useShowToast();
 
-    const [signInWithEmailAndPassword, user, loading, error,] = useSignInWithEmailAndPassword(auth);
+    const [signInWithEmailAndPassword, loading, error,] = useSignInWithEmailAndPassword(auth);
     const loginUser = useAuthStore((state) => state.login);
  
-    const login = async () => {
+    const login = async (inputs) => {
         if(!inputs.email || !inputs.password) {
             return showToast("Error", "Please fill all the fields", "error")
         }
@@ -22,12 +22,14 @@ const useLogin = () => {
                 const docRef = doc(firestore, "users", userCred.user.uid );
                 const docSnap = await getDoc(docRef);
                 localStorage.setItem("user-info",JSON.stringify(docSnap.data()))
+                loginUser(docSnap.data())
             }
 
         } catch (error) {
             showToast("Error", error.message, "error")
         }
     }
+    return {loading, error, login}
 }
 
 export default useLogin
