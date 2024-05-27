85fa4f6fb66a454990030a65685505bb77f055d2
Rahul Raj Anand <rahulrajanand7@gmail.com>
2024-05-27 12:38:26 +0530

    initial commit

--- ContraHub/src/components/AuthForm/AuthForm.jsx
+++ ContraHub/src/components/AuthForm/AuthForm.jsx
@@ -1,48 +1,46 @@
-import { Box, Flex, Image,Text, VStack } from "@chakra-ui/react"
-import { useState } from "react"
-import Signup from "./Signup"
-import Login from "./Login"
-import GoogleAuth from  "./GoogleAuth"
+
+import { Box, Flex, Image, Text, VStack } from "@chakra-ui/react";
+import { useState } from "react";
+import Login from "./Login";
+import Signup from "./Signup";
+import GoogleAuth from "./GoogleAuth";
 
 const AuthForm = () => {
-    const [isLogin, setIsLogin] = useState(true)    
-   
-  return (
-    <>
-        <Box border={"1px solid gray"} borderRadius={4} padding={5}>
-            <VStack spacing={4}>
-                <Image src="/logo.png" h={24} cursor={"pointer"} alt="ContraHub"/>
+	const [isLogin, setIsLogin] = useState(true);
 
-                {!isLogin ? <Login/> : <Signup/>}
+	return (
+		<>
+			<Box border={"1px solid gray"} borderRadius={4} padding={5}>
+				<VStack spacing={4}>
+					<Image src='/logo.png' h={24} cursor={"pointer"} alt='Instagram' />
 
-                {/**OR-------------- */}
-                <Flex alignItems={"center"} justifyContent={"center"} my={4} gap={1} w={"full"}>
-                    <Box flex={2} h={"1px"} bg={"gray.400"}/>
-                     <Text mx={1} color={"white"} >
-                        OR 
-                    </Text>   
-                    <Box flex={2} h={"1px"} bg={"gray.400"}/>
-                    </Flex>
+					{isLogin ? <Login /> : <Signup />}
 
-                    <GoogleAuth/>               
-            </VStack>
-        </Box>
+					{/* ---------------- OR -------------- */}
+					<Flex alignItems={"center"} justifyContent={"center"} my={4} gap={1} w={"full"}>
+						<Box flex={2} h={"1px"} bg={"gray.400"} />
+						<Text mx={1} color={"white"}>
+							OR
+						</Text>
+						<Box flex={2} h={"1px"} bg={"gray.400"} />
+					</Flex>
 
-        <Box border={"1px solid gray"} borderRadius={4} padding={5}>
-            <Flex alignItems={"center"} justifyContent={"center"}>
-                <Box mx={2} fontSize={14}>
+					<GoogleAuth prefix={isLogin ? "Log in" : "Sign up"} />
+				</VStack>
+			</Box>
 
-                   {isLogin ? "Don't have an account?": "Already have an account?" } 
-                </Box>
-                <Box onClick={() => setIsLogin(!isLogin)} color={"blue.500"} cursor={"pointer"}>
-                    {isLogin? "Sign Up": "Log in"}
-                </Box>
+			<Box border={"1px solid gray"} borderRadius={4} padding={5}>
+				<Flex alignItems={"center"} justifyContent={"center"}>
+					<Box mx={2} fontSize={14}>
+						{isLogin ? "Don't have an account?" : "Already have an account?"}
+					</Box>
+					<Box onClick={() => setIsLogin(!isLogin)} color={"blue.500"} cursor={"pointer"}>
+						{isLogin ? "Sign up" : "Log in"}
+					</Box>
+				</Flex>
+			</Box>
+		</>
+	);
+};
 
-            </Flex>
-            
-        </Box>
-    </>
-  )
-}
-
-export default AuthForm
+export default AuthForm;
