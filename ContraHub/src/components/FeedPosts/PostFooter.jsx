ee6250e41a26af5cc1786875271aaa13845bc85c
Rahul Raj Anand <rahulrajanand7@gmail.com>
2024-05-31 16:38:53 +0530

    commit

--- ContraHub/src/components/FeedPosts/PostFooter.jsx
+++ ContraHub/src/components/FeedPosts/PostFooter.jsx
@@ -1,10 +1,17 @@
 import { Box, Button, Flex, Input, InputGroup, InputRightElement, Text } from "@chakra-ui/react"
 import { useState } from "react"
 import { CommentLogo, NotificationsLogo, UnlikeLogo } from "../../assets/constants.jsx";
+import usePostComment from "../../hooks/usePostComment.js";
 
-const PostFooter = ( {username, isProfilePage} ) => {
+const PostFooter = ( {post,username, isProfilePage} ) => {
   const [liked, setLiked] = useState(false);
   const [likes, setLikes] = useState(1000);
+  const {isCommenting,handlePostComment} = usePostComment();
+  const [comment, setComment] = useState('')
+  const handleSubmitComment = async () => {
+    await handlePostComment(post.id,comment)
+    setComment('')
+  }
 
   const handleLike = () => {
     if(liked) {
@@ -55,7 +62,10 @@ const PostFooter = ( {username, isProfilePage} ) => {
     >
 
       <InputGroup>
-        <Input variant={"flushed"} placeholder={"add a comment"} fontSize={14}/>
+        <Input variant={"flushed"} placeholder={"Add a comment"} fontSize={14}
+          onChange={(e) => setComment(e.target.value)}
+          value={comment}
+        />
         <InputRightElement>
           <Button
             fontSize={14}
@@ -64,6 +74,8 @@ const PostFooter = ( {username, isProfilePage} ) => {
             cursor={"pointer"}
             _hover={{color:"white"}}
             bg={"transparent"}
+            onClick={handleSubmitComment}
+            isLoading={isCommenting}
           
           >Post</Button>
         </InputRightElement>
