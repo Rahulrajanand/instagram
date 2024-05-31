4045eebc6038042362f2b56f5d43042f140442aa
Rahul Raj Anand <rahulrajanand7@gmail.com>
2024-05-31 11:21:06 +0530

    init

--- ContraHub/src/components/Profile/ProfilePost.jsx
+++ ContraHub/src/components/Profile/ProfilePost.jsx
@@ -1,13 +1,37 @@
-import { Avatar, Box, Divider, Flex, GridItem, Image, Modal, ModalBody, ModalCloseButton, ModalContent,  ModalOverlay, Text, VStack, useDisclosure } from "@chakra-ui/react"
+import { Avatar, Box, Button, Divider, Flex, GridItem, Image, Modal, ModalBody, ModalCloseButton, ModalContent,  ModalOverlay, Text, VStack, useDisclosure } from "@chakra-ui/react"
 import { AiFillHeart } from 'react-icons/ai';
 import { FaComment } from 'react-icons/fa';
 import { MdDelete } from 'react-icons/md';
 import Comment from "../Comment/Comment";
 import PostFooter from "../FeedPosts/PostFooter";
+import useUserProfileStore from "../../store/userProfileStore";
+import useAuthStore from '../../store/authStore'
+import useShowToast from '../../hooks/useShowToast'
+import { useState } from "react";
+import { deleteObject,ref } from 'firebase/storage';
+import { storage } from '../../firebase/firebase'
  
 
 const ProfilePost = ( {post} ) => {
   const { isOpen, onOpen, onClose } = useDisclosure()
+  const userProfile = useUserProfileStore((state) => state.userProfile);
+  const authUser = useAuthStore((state) => state.user);
+  const showToast = useShowToast();
+  const [isDeleting, setIsDeleting] = useState(false);
+  
+  const handleDeletePost = async () => {
+    if(!window.confirm("Are you sure you want to delete this post?")) return;
+    try {
+      const imageRef = ref(storage, `posts/${post.id}`);
+      await deleteObject(imageRef);
+
+
+
+    } catch (error) {
+      showToast("Error", error.message, "error")
+    }
+  }
+
   console.log(post);
   return (
     <>
@@ -41,13 +65,13 @@ const ProfilePost = ( {post} ) => {
       <Flex>
         <AiFillHeart size={20}/>
         <Text fontWeight={"bold"} ml={2}>
-            7
+            {post.likes.length}
         </Text>
       </Flex>
       <Flex>
-      <FaComment size={20}/>
+      <FaComment size={20}/> 
         <Text fontWeight={"bold"} ml={2}>
-          7
+          {post.comments.length}
          </Text>
        </Flex>
       </Flex>
@@ -58,31 +82,44 @@ const ProfilePost = ( {post} ) => {
 
     <Modal isOpen={isOpen} onClose={onClose}
       isCentered={true}
-      size={{base:"3xl",md:"5xl"}}
-    
+      size={{base:"3xl",md:"5xl"}}    
     >
         <ModalOverlay />
         <ModalContent>
           {/* <ModalHeader/> */}
           <ModalCloseButton />
           <ModalBody bg={"black"} pb={5}>
-            <Flex gap="4" w={{base:"90%",sm: "70%", md: "full" }} mx={"auto"}>
-              <Box borderRadius={4} overflow={"hidden"} border={"1px solid"} borderColor={"whiteAlpha.300"} flex={1.5}>
+            <Flex gap="4" w={{base:"90%",sm: "70%", md: "full" }} mx={"auto"}
+              maxH={"90vh"}
+              minH={"50vh"}
+            >
+              <Flex borderRadius={4} overflow={"hidden"} border={"1px solid"} borderColor={"whiteAlpha.300"} flex={1.5}
+                justifyContent={"center"}
+                alignItems={"center"}
+              >
 
               <Image src={post.imageURL} alt="profile post"/>
-              </Box>
+              </Flex>
               <Flex flex={1} flexDir={"column"} px={10} display={{base: "none", md:"flex"}}>
                 <Flex alignItems={"center"} justifyContent={"space-between"}>               
                   <Flex alignItems={"center"} gap={4}>
-                    <Avatar src='/profilepic.png' size={"sm"} name="as a prohragmmer"/>
+                    <Avatar src={userProfile.profilePicURL} size={"sm"} name="as a prohragmmer"/>
                       <Text fontWeight={"bold"} fontSize={12}>
-                        Rahul
+                        {userProfile.username}
                       </Text>
                   </Flex>
                
-              <Box _hover={{bg:"whiteAlpha.300",color:"red.600"}} borderRadius={4} p={1}>
+              {authUser?.uid === userProfile.uid && (
+                <Button 
+                size={"sm"}
+                bg={"transparent"}
+              _hover={{bg:"whiteAlpha.300",color:"red.600"}} borderRadius={4} p={1}
+              onClick={handleDeletePost}
+              >
+
                 <MdDelete size={20} cursor="pointer"/>
-              </Box>
+              </Button>
+              )}
             </Flex>
             <Divider my={4} bg={"gray.500"}/>
 
@@ -94,19 +131,6 @@ const ProfilePost = ( {post} ) => {
               text={"Dummy images from unsplash"}
               />
 
-              <Comment
-              createdAt="12h ago"
-              username="ronaldo"
-              profilePic={"https://bit.ly/dan-abramov"}
-              text={"nice pic"}
-              />
-
-              <Comment
-              createdAt="1h ago"
-              username="messo"
-              profilePic={"https://bit.ly/kent-c-dodds"}
-              text={"Good work"}
-              />
             </VStack>
             <Divider my={4} bg={"gray.800"}/>
 
--- ContraHub/src/store/postStore.js
+++ ContraHub/src/store/postStore.js
@@ -2,7 +2,9 @@ import {create} from "zustand"
 
 const usePostStore = create((set) => ({
     posts: [],
-    createPost: (post) => set(state => ({posts:[post, ...state.posts] }))
+    createPost: (post) => set(state => ({posts:[post, ...state.posts] })),
+
+    deletepost : (id) => set(state => ({posts: state.posts.filter(post => post.id !== id)})),
 
     setPosts: (posts) => set({ posts }), 
 }))
