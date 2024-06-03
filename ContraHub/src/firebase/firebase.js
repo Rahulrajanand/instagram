bcebab743fcf91feb0029f0e453f35bfe02dd194
Rahul Raj Anand <rahulrajanand7@gmail.com>
2024-06-03 19:22:16 +0530

    commmit

--- ContraHub/src/firebase/firebase.js
+++ ContraHub/src/firebase/firebase.js
@@ -1,16 +1,17 @@
 import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth';
-import {getFirestore} from 'firebase/firestore';
-import {getStorage} from 'firebase/storage';
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAXBTwPEOp_WvOQsWPLdnYb6KdB-2WSOpI",
-  authDomain: "insta-clone-8eb8f.firebaseapp.com",
-  projectId: "insta-clone-8eb8f",
-  storageBucket: "insta-clone-8eb8f.appspot.com",
-  messagingSenderId: "38206809398",
-  appId: "1:38206809398:web:730c99e13c5b680c01ff97",
-  measurementId: "G-HV23S8VMQP"
+	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+	storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+	appId: import.meta.env.VITE_FIREBASE_APP_ID,
+	measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
@@ -18,4 +19,4 @@ const auth = getAuth(app);
 const firestore = getFirestore(app);
 const storage = getStorage(app);
 
-export {app, auth, firestore, storage};
+export { app, auth, firestore, storage };
